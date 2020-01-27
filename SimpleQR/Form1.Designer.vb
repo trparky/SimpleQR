3fca5d6f60116a4a8b7e2665e42feebc7c2d5da6
Tom Parkison <trparky@toms-world.org>
2020-01-27 13:20:16 -0500

    Fixed minimum size.

--- SimpleQR/Form1.Designer.vb
+++ SimpleQR/Form1.Designer.vb
@@ -211,7 +211,7 @@ Partial Class Form1
         Me.Controls.Add(Me.btnQRCodeBuilder)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
-        Me.MinimumSize = New System.Drawing.Size(535, 398)
+        Me.MinimumSize = New System.Drawing.Size(543, 404)
         Me.Name = "Form1"
         Me.Text = "SimpleQR"
         CType(Me.qrCodeImage, System.ComponentModel.ISupportInitialize).EndInit()
