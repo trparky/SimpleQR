ef9f0272a204c850434ff42f25b0a9a70af83d12
Thomas Parkison <trparky@toms-world.org>
2022-11-24 18:51:33 -0500

    Added a file to be ignored by GIT.

--- Compile, package, and upload this thing!.bat
+++
@@ -1,4 +0,0 @@
-MSBuild.exe "SimpleQR.sln" /noconsolelogger /t:Rebuild /p:Configuration=Debug
-MSBuild.exe "SimpleQR.sln" /noconsolelogger /t:Rebuild /p:Configuration=Release
-cd "SimpleQR\bin\Release"
-"Package This Program!.bat"
