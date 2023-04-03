3740683fa4770e97adf7c1db424082ae88c74fb3
Thomas Parkison <trparky@toms-world.org>
2023-04-03 18:55:28 -0400

    Removed some unneeded native methods.

--- SimpleQR/Program Modules/NativeMethod.vb
+++ SimpleQR/Program Modules/NativeMethod.vb
@@ -2,10 +2,6 @@
 
 Namespace NativeMethod
     Friend Class NativeMethods
-        <DllImport("User32.dll", EntryPoint:="SetForegroundWindow")>
-        Public Shared Function SetForegroundWindow(hWnd As Integer) As Integer
-        End Function
-
         <DllImport("kernel32.dll")>
         Friend Shared Function OpenProcess(dwDesiredAccess As ProcessAccessFlags, bInheritHandle As Boolean, dwProcessId As Integer) As IntPtr
         End Function
@@ -17,17 +13,6 @@ Namespace NativeMethod
         <DllImport("kernel32.dll", CharSet:=CharSet.Unicode)>
         Friend Shared Function QueryFullProcessImageName(hprocess As IntPtr, dwFlags As Integer, lpExeName As Text.StringBuilder, ByRef size As Integer) As Boolean
         End Function
-
-        <DllImport("shell32.dll", CharSet:=CharSet.Unicode, ExactSpelling:=True)>
-        Public Shared Function ILCreateFromPathW(pszPath As String) As IntPtr
-        End Function
-        <DllImport("shell32.dll", ExactSpelling:=True)>
-        Public Shared Function SHOpenFolderAndSelectItems(pidlList As IntPtr, cild As UInteger, children As IntPtr, dwFlags As UInteger) As Integer
-        End Function
-
-        <DllImport("shell32.dll", ExactSpelling:=True)>
-        Public Shared Sub ILFree(pidlList As IntPtr)
-        End Sub
     End Class
 
     Module APIs
