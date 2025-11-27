a45a04162c336e5f19ba1fbe64107f4bf3894c29
Thomas Parkison <trparky@toms-world.org>
2025-11-26 21:30:23 -0500

    Added a null string check.

--- SimpleQR/Bigger QRCode Image.vb
+++ SimpleQR/Bigger QRCode Image.vb
@@ -2,19 +2,21 @@
     Public textToEncode As String
 
     Sub GenerateImage()
-        Try
-            Dim writer As New ZXing.BarcodeWriter
-            With writer
-                .Options.Width = qrCodeImage.Size.Width
-                .Options.Height = qrCodeImage.Size.Height
-                .Options.PureBarcode = True
-                .Options.Margin = 0
-                .Format = ZXing.BarcodeFormat.QR_CODE
-                qrCodeImage.Image = .Write(textToEncode)
-            End With
-        Catch ex As ArgumentException
-        Catch ex As Exception
-        End Try
+        If Not String.IsNullOrWhiteSpace(textToEncode) Then
+            Try
+                Dim writer As New ZXing.BarcodeWriter
+                With writer
+                    .Options.Width = qrCodeImage.Size.Width
+                    .Options.Height = qrCodeImage.Size.Height
+                    .Options.PureBarcode = True
+                    .Options.Margin = 0
+                    .Format = ZXing.BarcodeFormat.QR_CODE
+                    qrCodeImage.Image = .Write(textToEncode)
+                End With
+            Catch ex As ArgumentException
+            Catch ex As Exception
+            End Try
+        End If
     End Sub
 
     Private Sub BigImage_Load(sender As Object, e As EventArgs) Handles MyBase.Load
